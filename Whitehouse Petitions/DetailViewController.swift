ce7c979765920a0afe56aea493d6af72a62f9703
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-10-27 16:07:31 +0700

    Setup the logic to render petition content from the web to a web view.

--- Whitehouse Petitions/DetailViewController.swift
+++ Whitehouse Petitions/DetailViewController.swift
@@ -7,32 +7,39 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
+    // MARK: - Stored Properties
+    
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    // MARK: - Methods Override
+    
+    override func loadView() {
+        self.webView = WKWebView()
+        self.view = self.webView
     }
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
+        
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
